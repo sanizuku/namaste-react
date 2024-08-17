61ee011a2d7a6af4ffd426be92b3dd2505a14279
sandeep <sandeeps16xgmail.com>
2024-08-17 21:09:23 +0530

    Updates

--- Grocery.e3444452.js
+++
@@ -1,2 +0,0 @@
-var e=globalThis.parcelRequireb877;(0,e.register)("loR3J",function(o,r){Object.defineProperty(o.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(o.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),t=()=>(0,i.jsx)("h1",{children:"Suppose 1000s of component so this is chuncking"})});
-//# sourceMappingURL=Grocery.e3444452.js.map
